36b6f5974ca2e9a3c2dbdc537678260256d293e7
Olga <tiutiunnikolga@gmail.com>
2025-10-31 18:05:35 +0300

    feature_5

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from blog.apps import BlogConfig
 from blog.views import BlogView, CreatePostView, BlogListView, BlogDetailView
 
@@ -8,5 +10,5 @@ urlpatterns = [
     path('blog_list/', BlogView.as_view(), name='blog_list'),
     path('create_post_view/', CreatePostView.as_view(), name='create_post'),
     path('page/', BlogListView.as_view(), name='page'),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<int:pk>/', cache_page(60)(BlogDetailView.as_view()), name='blog_detail'),
 ]
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from catalog.apps import ProductsConfig
-from catalog.views import ProductViews, ProductDetailView, ContactView, ProductUpdateView, ProductCreateView, ProductDeleteView
+from catalog.views import ProductViews, ProductDetailView, ContactView, ProductUpdateView, ProductCreateView, \
+    ProductDeleteView, ProductCategoryListView
+from catalog.sevices import ProductService
 
 app_name = ProductsConfig.name
 
@@ -10,5 +12,6 @@ urlpatterns = [
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('create/', ProductCreateView.as_view(), name='product_create'),
     path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('product_catalog/<int:category_id>/', ProductCategoryListView.as_view(), name='product_catalog')
 ]
